6d00325e8952807986edacaa5b6ba79c561af59c
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2025-02-21 19:49:25 -0300

    feat:✨responsive menu & non-responsive main

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,9 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding: "1rem",
       screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1736px",
+        "2xl": "1636px",
       },
     },
     extend: {
